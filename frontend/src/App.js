a8e358824e2aa4c6519ccc1e687319a527c0cc5c
MoetassemCh <103120209+MoetassemCh@users.noreply.github.com>
2022-10-23 17:31:26 +0300

    100%fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 
+import { useEffect } from "react";
 import {BrowserRouter,Routes,Route,useNavigate} from "react-router-dom"
 import Home from "./container/Home";
 import Login from "./container/Login";
+import { fetchUser } from "./utils/fetchUser";
 
 function App() {
+
+  const navigate=useNavigate()
+
+  useEffect(() => {
+    const user=fetchUser()
+if(!user) navigate('/login')
+  }, [])
+  
   return (
     <BrowserRouter>
       <Routes>
